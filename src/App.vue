a6bfe83d0327c9a9e8a9f5d435baec153a58d787
gsolo <g.solomonidze@wonderland.ge>
2019-07-03 13:32:02 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat to="/">
+        <span class="mr-2">Home</span>
+      </v-btn>
+      <v-btn flat to="/product">
+        <span class="mr-2">Product</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      {{categories}}
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from "./components/HelloWorld";
+import { mapState } from "vuex";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  methods: {},
+  computed: mapState(["categories"]),
+  mounted() {
+    this.$store.dispatch("getCategories");
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    categories: []
   },
   mutations: {
-
+    SET_CATEGORIES(state, categories) {
+      state.categories = categories;
+    }
   },
   actions: {
-
+    getCategories({ commit }, categories) {
+      axios.get("https://backendapi.turing.com/departments").then(res => {
+        commit("SET_CATEGORIES", res.data);
+      });
+    }
   }
-})
+});
